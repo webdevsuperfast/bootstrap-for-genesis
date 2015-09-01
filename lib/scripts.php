2a5e702e212d14c8a6463b8a1a093c05f31eb012
SuperFastBusiness Team <webdevsuperfast@gmail.com>
2015-09-01 08:36:16 +0800

    Removed genesis stylesheet callback and added separate stylesheet

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,15 +11,19 @@
  *
 */
 
-// Theme Scripts
+// Theme Scripts & Stylesheet
+remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
 add_action( 'wp_enqueue_scripts', 'bfg_theme_scripts' );
 function bfg_theme_scripts() {
-
+	$version = wp_get_theme()->Version;
 	if ( !is_admin() ) {
-		wp_register_script( 'vendor-js', BFG_THEME_SCRIPTS . 'vendor.min.js', array( 'jquery' ), null, true );
+		wp_register_style( 'bootstrap_for_genesis_styles', BFG_THEME_CSS . 'style.min.css', array(), $version );
+		wp_enqueue_style( 'bootstrap_for_genesis_styles' );
+		
+		wp_register_script( 'vendor-js', BFG_THEME_JS . 'vendor.min.js', array( 'jquery' ), null, true );
 		wp_enqueue_script( 'vendor-js' );
 
-		wp_register_script( 'app-js', BFG_THEME_SCRIPTS . 'app.min.js', array( 'jquery' ), null, true );
+		wp_register_script( 'app-js', BFG_THEME_JS . 'app.min.js', array( 'jquery' ), null, true );
 		wp_enqueue_script( 'app-js' );
 	}
 }
