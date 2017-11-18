d577b08170bf46452007cf4c2e4b808962bb4ed7
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2017-11-18 09:12:23 +0800

    Removed unneeded line of codes

--- assets/js/source/app.js
+++ assets/js/source/app.js
@@ -2,15 +2,4 @@
 	$(document).ready(function(){
 
 	});
-
-	// Window load event with minimum delay
-	// @https://css-tricks.com/snippets/jquery/window-load-event-with-minimum-delay/
-	(function fn() {
-		fn.now = +new Date;
-		$(window).load(function() {
-			if (+new Date - fn.now < 500) {
-				setTimeout(fn, 500);
-			}
-		});
-	})();
 })(jQuery);
