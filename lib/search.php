3f5282169d7a95b29a65db00e07123d79f39ac00
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2018-02-04 08:03:29 +0800

    Renamed package name

--- lib/search.php
+++ lib/search.php
@@ -2,7 +2,7 @@
 /**
  * Search Form
  *
- * @package      B4Genesis
+ * @package      Bootstrap for Genesis
  * @since        1.0
  * @link         http://rotsenacob.com
  * @author       Rotsen Mark Acob <rotsenacob.com>
