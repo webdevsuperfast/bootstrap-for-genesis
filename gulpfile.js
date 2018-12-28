748c0ac3a6c366d44bd8f644a494aa3c09f818da
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2018-12-28 12:23:06 +0800

    Use my own build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,102 @@
-'use strict';
-
 var gulp = require('gulp'),
-    pkg = require('./package.json'),
-    toolkit = require('gulp-wp-toolkit');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-clean-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    prettify = require('gulp-jsbeautifier'),
+    vinylpaths = require('vinyl-paths'),
+    cmq = require('gulp-combine-mq'),
+    merge = require('merge-stream'),
+    foreach = require('gulp-flatmap'),
+    changed = require('gulp-changed'),
+    browserSync = require('browser-sync'),
+    del = require('del');
 
-toolkit.extendConfig({
-    theme: {
-        name: pkg.theme.name,
-		themeuri: pkg.homepage,
-		description: pkg.theme.description,
-		author: pkg.author,
-		authoruri: pkg.theme.authoruri,
-		version: pkg.version,
-		license: pkg.license,
-		licenseuri: pkg.theme.licenseuri,
-		tags: pkg.theme.tags,
-		textdomain: pkg.name,
-		domainpath: pkg.theme.domainpath,
-		template: 'genesis',
+var paths = {
+    styles: {
+        src: 'assets/scss/style.scss',
+        dest: 'assets/css'
     },
-    css: {
-        basefontsize: 16, // Used by postcss-pxtorem.
-		cssnano: {
-			discardComments: {
-				removeAll: true
-			},
-			zindex: false,
-		},
-        remreplace: false, // Used by postcss-pxtorem.
-        remmediaquery: false, // Used by postcss-pxtorem.
-		scss: {
-            'smartmenus-bootstrap': {
-                src: 'node_modules/smartmenus-bootstrap-4/jquery.smartmenus.bootstrap-4.css',
-                dest: 'assets/css/',
-                outputStyle: 'compressed',
-                sourceMap: true
-            }
-		},
-    },
-    js: {
-        'app': [
-            'develop/js/source/*.js'
-        ],
-        'jquery': [
-            'node_modules/jquery/dist/jquery.js'
-        ],
-        'popper': [
-            'node_modules/popper.js/dist/umd/popper.js'
-        ],
-        'bootstrap': [
-            'node_modules/bootstrap/dist/js/bootstrap.js'
-        ],
-        'smartmenus': [
-            'node_modules/smartmenus/dist/jquery.smartmenus.js'
-        ],
-        'smartmenus-bootstrap': [
+    scripts: {
+        src: [
+            'assets/js/source/app.js',
+            'node_modules/jquery/dist/jquery.slim.js',
+            'node_modules/bootstrap/dist/js/bootstrap.js',
+            'node_modules/popper.js/dist/umd/popper.js',
+            'node_modules/smartmenus/dist/jquery.smartmenus.js',
             'node_modules/smartmenus-bootstrap-4/jquery.smartmenus.bootstrap-4.js'
-        ]
-    },
-    dest: {
-        js: 'assets/js/',
-        css: 'assets/css/'
-    },
-    server: {
-        proxy: 'wordpress.test',
-        online: true
+        ],
+        dest: 'assets/js'
     }
-});
+}
 
-toolkit.extendTasks(gulp, /* Gulp task overrides. */);
+function scriptsLint() {
+    return gulp.src('assets/js/**/*','gulpfile.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+}
+
+function style() {
+    var cssStream = gulp.src('node_modules/smartmenus-bootstrap-4/jquery.smartmenus.bootstrap-4.css')
+        .pipe(concat('smartmenus.css'));
+    
+    var sassStream = gulp.src(paths.styles.src)
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(concat('app.scss'));
+    
+    var mergeStream = merge(sassStream, cssStream)
+        .pipe(concat('app.css'))
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(cmq())
+        .pipe(rename('app.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream({match: '**/*.css'}))
+        .pipe(notify({ message: 'Styles task complete' }));
+    
+    return mergeStream;
+}
+
+function js() {
+    return gulp.src(paths.scripts.src)
+    .pipe(changed('js'))
+    .pipe(foreach(function(stream, file){
+        return stream
+            .pipe(uglify())
+            .pipe(rename({suffix: '.min'}))
+    }))
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(notify({ message: 'Scripts task complete' }));
+}
+
+function browserSyncServe(done) {
+    browserSync.init({
+        injectChanges: true,
+        proxy: 'http://wordpress.test'
+    })
+    done();
+}
+
+function browserSyncReload(done) {
+    browserSync.reload();
+    done();
+}
+
+function watch() {
+    gulp.watch(paths.styles.src, style).on('change', browserSync.reload)
+    gulp.watch(paths.scripts.src, gulp.series(scriptsLint, js))
+    gulp.watch([
+            '*.php',
+            'lib/*'
+        ],
+        gulp.series(browserSyncReload)
+    )
+}
+
+gulp.task('default', gulp.parallel(style, js, browserSyncServe, watch));
