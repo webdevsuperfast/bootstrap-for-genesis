3b7a44f2904403c7b6b113285a40824b383a0956
Rotsen Mark Acob <webdevsuperfast@devil-hunter.localdomain>
2020-05-13 08:49:49 +0800

    Changed icon to SVG bootstrap icon

--- lib/search.php
+++ lib/search.php
@@ -15,7 +15,10 @@ add_filter( 'get_search_form', 'bfg_search_form' );
 function bfg_search_form( $form ) {
     $form = '<form class="form-inline" role="search" method="get" id="searchform" action="' . home_url('/') . '" >
 	<input class="form-control mr-2" type="text" value="' . get_search_query() . '" placeholder="' . esc_attr__('Search', 'bootstrap-for-genesis') . '..." name="s" id="s" />
-	<button type="submit" id="searchsubmit" value="'. esc_attr__('Search', 'bootstrap-for-genesis') .'" class="btn btn-primary"><i class="fa fa-search"></i></button>
+	<button type="submit" id="searchsubmit" value="'. esc_attr__('Search', 'bootstrap-for-genesis') .'" class="btn btn-primary"><svg class="bi bi-search" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clip-rule="evenodd"/>
+        <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clip-rule="evenodd"/>
+      </svg></button>
     </form>';
     return $form;
 }
